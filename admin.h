f09d304507b2040643e35025d6c7e1f847acbee9
nadaalaa289 <168455762+nadaalaa289@users.noreply.github.com>
2024-05-09 23:55:53 +0300

    Delete admin.h

--- admin.h
+++
@@ -1,9 +0,0 @@
-#include "student_database.h"
-#include"edit.h"
-#ifndef NADA_ADMIN_H
-#define NADA_ADMIN_H
-void remove_student_data(int);
-int edit_admin_password();
-void edit_student_grade(int);
-
-#endif //NADA_ADMIN_H
