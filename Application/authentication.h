40fde369f5edde9a6d68d977253c6513164b4189
nadaalaa289 <168455762+nadaalaa289@users.noreply.github.com>
2024-05-10 20:19:41 +0300

    Delete Application directory

--- Application/authentication.h
+++
@@ -1,7 +0,0 @@
-#include "file_access.h"
-
-#ifndef C_PROJECT_AUTHENTICATION_H
-#define C_PROJECT_AUTHENTICATION_H
-int authenticate_admin(const char *);
-int authenticate_student(const char *);
-#endif //C_PROJECT_AUTHENTICATION_H
